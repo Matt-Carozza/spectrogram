cb808d2f2ac7512f87b869f43d1e7947c96428a4
Matt Carozza <mcar728@gmail.com>
2026-01-05 11:47:33 -0500

    Handled final sample of song file

--- spectrogram.c
+++ spectrogram.c
@@ -7,12 +7,11 @@
 
 /*
 TODO:
-- Find way to fill one extra buffer (make sure to apply fft again)
 - Possibly change name of ft_create (seems vague)
 */
 
 typedef struct {
-    float* amplitude_size;
+    size_t amplitude_size;
     float* amplitude;
     float* frequency;
 } FreqDomain;
@@ -48,7 +47,8 @@ int main(void) {
 
 
 void fd_create(FreqDomain* fd, Wave* wave) {
-    fd->amplitude = malloc(wave->frameCount * sizeof(fd->amplitude));
+    fd->amplitude_size = 0;
+    fd->amplitude = malloc((wave->frameCount + 1) * sizeof(fd->amplitude));
     fd->frequency = malloc(BUFFER_SIZE * sizeof(fd->frequency));
 }
 
@@ -63,21 +63,30 @@ void ft_create(Wave* wave, FreqDomain* fd) {
     short PCM_buffer[BUFFER_SIZE];
     double complex fft_output[BUFFER_SIZE];
     size_t wave_iterator = 0;
-    size_t amplitude_iterator = 0;
     short* wave_data_ptr = wave->data;
     
-    for(wave_iterator = 0; wave_iterator < wave->frameCount - BUFFER_SIZE; ++wave_iterator) {
+    for(wave_iterator = 0; wave_iterator < wave->frameCount - BUFFER_SIZE;) {
         for(size_t j = 0; j < BUFFER_SIZE; ++j) {
             PCM_buffer[j] = wave_data_ptr[wave_iterator++];
         }
         fft(PCM_buffer, fft_output, BUFFER_SIZE);
         for(size_t j = 0; j < BUFFER_SIZE; ++j) {
-            fd->amplitude[amplitude_iterator++] = fft_output[j];
+            fd->amplitude[fd->amplitude_size++] = fft_output[j];
         }
     }
- 
-    for(size_t i = 0; i < wave->frameCount % BUFFER_SIZE; ++i) {
-        PCM_buffer[i] = wave_data_ptr[wave_iterator++];
+    
+    // Final buffer will exceed frame count size, so zeros must be put into after
+    for(size_t i = 0; i < BUFFER_SIZE; ++i) {
+        if (i < wave->frameCount % BUFFER_SIZE) {
+            PCM_buffer[i] = wave_data_ptr[wave_iterator++];
+        } 
+        else  {
+            PCM_buffer[i] = 0;
+        }
+    }
+    fft(PCM_buffer, fft_output, BUFFER_SIZE);
+    for(size_t i = 0; i < BUFFER_SIZE; ++i) {
+        fd->amplitude[fd->amplitude_size++] = fft_output[i];
     }
 }
 

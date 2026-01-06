f72e01f5f81fa502aaaaca219f55465aa758f9bb
Matt Carozza <mcar728@gmail.com>
2026-01-06 16:28:20 -0500

    Split amplitude in half

--- spectrogram.c
+++ spectrogram.c
@@ -4,27 +4,36 @@
 #include <raylib.h>
 #include <complex.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 /*
 TODO:
-- Possibly change name of ft_create (seems vague)
+- Segmentation 
+- Windowing Signal
 */
+
+#define SAMPLE_RATE 44100 
+#define BUFFER_SIZE 4096
+#define HALF_BUFFER_SIZE (BUFFER_SIZE / 2)
+#define PI 3.14159265358979323846f
 
 typedef struct {
     size_t amplitude_size;
-    float* amplitude;
-    float* frequency;
+    size_t sample_size;
+    double* amplitude;
+    double* frequency;
 } FreqDomain;
 
 // MEL DATA STRUCT
 
-#define SAMPLE_RATE 44100 
-#define BUFFER_SIZE 4096
-#define PI 3.14159265358979323846f
 
 void fd_create(FreqDomain* fd, Wave* wave);
 void fd_destroy(FreqDomain* fd);
-void ft_create(Wave* wave, FreqDomain* fd);
+void fd_set_frequency(FreqDomain* fd);
+void fd_set_amplitude(FreqDomain* fd, Wave* wave);
+void fd_write_buffer_to_file(FreqDomain* fd, size_t sample_index);
 void fft(short in[], double complex out[], size_t n);
 void _fft(short in[], double complex out[], size_t n, size_t stride);
 
@@ -37,7 +46,9 @@ int main(void) {
 
     fd_create(&fd, &wave_copy);
 
-    ft_create(&wave_copy, &fd);
+    fd_set_amplitude(&fd, &wave_copy);
+    fd_set_frequency(&fd);
+    fd_write_buffer_to_file(&fd, fd.sample_size / 2);
 
     fd_destroy(&fd);
 
@@ -48,8 +59,9 @@ int main(void) {
 
 void fd_create(FreqDomain* fd, Wave* wave) {
     fd->amplitude_size = 0;
-    fd->amplitude = malloc((wave->frameCount + 1) * sizeof(fd->amplitude));
-    fd->frequency = malloc(BUFFER_SIZE * sizeof(fd->frequency));
+    fd->sample_size = 0;
+    fd->amplitude = malloc((wave->frameCount + HALF_BUFFER_SIZE) * sizeof(fd->amplitude));
+    fd->frequency = malloc(HALF_BUFFER_SIZE * sizeof(fd->frequency));
 }
 
 void fd_destroy(FreqDomain* fd) {
@@ -59,20 +71,29 @@ void fd_destroy(FreqDomain* fd) {
     fd->frequency = NULL;
 }
 
-void ft_create(Wave* wave, FreqDomain* fd) {
+void fd_set_frequency(FreqDomain* fd) {
+    for(size_t i = 0; i < HALF_BUFFER_SIZE; i++) {
+        fd->frequency[i] = ((double)i / HALF_BUFFER_SIZE) * SAMPLE_RATE;
+    }
+}
+
+void fd_set_amplitude(FreqDomain* fd, Wave* wave) {
     short PCM_buffer[BUFFER_SIZE];
     double complex fft_output[BUFFER_SIZE];
     size_t wave_iterator = 0;
     short* wave_data_ptr = wave->data;
+    double debug_amplitude[HALF_BUFFER_SIZE];
     
     for(wave_iterator = 0; wave_iterator < wave->frameCount - BUFFER_SIZE;) {
         for(size_t j = 0; j < BUFFER_SIZE; ++j) {
             PCM_buffer[j] = wave_data_ptr[wave_iterator++];
         }
         fft(PCM_buffer, fft_output, BUFFER_SIZE);
-        for(size_t j = 0; j < BUFFER_SIZE; ++j) {
-            fd->amplitude[fd->amplitude_size++] = fft_output[j];
+        for(size_t j = 0; j < HALF_BUFFER_SIZE; ++j) {
+            fd->amplitude[fd->amplitude_size++] = cabs(fft_output[j]);
+            debug_amplitude[j] = cabs(fft_output[j]);
         }
+        fd->sample_size++;
     }
     
     // Final buffer will exceed frame count size, so zeros must be put into after
@@ -85,11 +106,36 @@ void ft_create(Wave* wave, FreqDomain* fd) {
         }
     }
     fft(PCM_buffer, fft_output, BUFFER_SIZE);
-    for(size_t i = 0; i < BUFFER_SIZE; ++i) {
-        fd->amplitude[fd->amplitude_size++] = fft_output[i];
+    for(size_t i = HALF_BUFFER_SIZE - 1; i < BUFFER_SIZE; ++i) {
+        fd->amplitude[fd->amplitude_size++] = cabs(fft_output[i]);
     }
+    fd->sample_size++; // Increase sample by one for final buffer
+    fd += 0;
 }
 
+void fd_write_buffer_to_file(FreqDomain* fd, size_t sample_index) {
+    FILE *file;
+    
+    struct stat st = {0};
+    
+    file = fopen("debug/sample.txt", "w");
+    
+    if (file ==  NULL) {
+       perror("Error Opening File");
+       return;
+    }
+    
+    for (size_t i = 0; i < HALF_BUFFER_SIZE; ++i) {
+        fprintf(file, "%zu: (%lfHz, %lf)\n", i, fd->frequency[i], fd->amplitude[(sample_index * HALF_BUFFER_SIZE) + i]);
+    }
+
+    // for (size_t i = 0; i < HALF_BUFFER_SIZE; ++i) {
+    //     fprintf(file, "(%zu, %lfHz): (%lf, %lf)\n", i, fd->frequency[HALF_BUFFER_SIZE - i - 1], fd->amplitude[(HALF_BUFFER_SIZE * sample_index) + i], fd->amplitude[(HALF_BUFFER_SIZE * (sample_index + 1)) - i]);
+    // }
+    
+    fclose(file);
+    return;
+}
 
 void fft(short in[], double complex out[], size_t n) {
     assert(n % 2 == 0);
